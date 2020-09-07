1194be96af65564860bc5780c284107710ad89b5
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2020-09-07 22:55:09 +0500

    restructuring

--- routes/role.js
+++ routes/role.js
@@ -1,15 +1,17 @@
-const Role = require("../models/role");
-const express = require("express");
-const app = express();
+const Role = require("../models/role")
+const express = require("express")
+const app = express()
 
-module.exports = async (app) => {
+module.exports = async app => {
   app.post("/api/role", async (req, res) => {
     try {
-      let role = new Role(req.body);
-      role = await role.save();
-      res.status(200).send(role);
+      let role = new Role(req.body)
+      role = await role.save()
+      res.status(200).send(role)
     } catch (err) {
-      res.status(406).send({ message: err.message });
+      res.status(406).send({ message: err.message })
     }
-  });
-};
+  })
+
+  // app.post("/api")
+}
--- startup/routes.js
+++ startup/routes.js
@@ -1,16 +1,18 @@
-const express = require("express");
-const product = require("../routes/product");
-const order = require("../routes/order");
+const express = require("express")
+const product = require("../routes/product")
+const order = require("../routes/order")
 
-module.exports = (app) => {
-  app.use("/uploads", express.static("uploads"));
-  app.use(express.json());
+module.exports = app => {
+  app.use("/uploads", express.static("uploads"))
+  app.use(express.json())
 
   app.get("/", async (req, res) => {
-    res.status(200).send("Server is working");
-  });
+    res.status(200).send("Server is working")
+  })
+
+  app.use("/api")
   // require("../middleware/auth");
-  require("../routes/role")(app);
-  require("../routes/users")(app);
-  require("../routes/authentication")(app);
-};
+  require("../routes/role")(app)
+  require("../routes/users")(app)
+  require("../routes/authentication")(app)
+}
